c06c97c134850a138cb7fe89902bb6b434922646
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-11-14 15:39:31 -0300

    update 3

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cel-fii-frontend/",
-
+  base: "/", 
   build: {
     outDir: "dist",
   },
-
   server: {
     historyApiFallback: true,
   },
