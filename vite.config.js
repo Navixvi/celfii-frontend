ea89b5171c7d8b07f7ff86865fdc051099376621
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-11-14 15:35:36 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: "/",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
-  server: {
-    historyApiFallback: true,
-  },
+
 });
