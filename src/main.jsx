556329bbdce8b355bb70e35462a37a789218efea
María Sol <mikaiha888@gmail.com>
2024-11-14 15:12:56 -0300

    chore: change vite and main router config

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import axios from "axios";
 import store from "./redux";
@@ -9,14 +9,14 @@ import store from "./redux";
 import App from "./App.jsx";
 import "./index.css";
 
-axios.defaults.baseURL = "https://celfii-backend.up.railway.app";
+axios.defaults.baseURL = "https://celfii-backend.up.railway.app/";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <BrowserRouter>
+				<App />
+			</BrowserRouter>
     </Provider>
   </StrictMode>
 );
