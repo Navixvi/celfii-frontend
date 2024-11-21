293222ebb7c9eaf5197b7769d38a8a969ac40a13
María Sol <mikaiha888@gmail.com>
2024-11-21 14:25:11 -0300

    fix: category name

--- src/view/home-page/home-sections/HeroBanner.jsx
+++ src/view/home-page/home-sections/HeroBanner.jsx
@@ -5,8 +5,8 @@ const HeroBanner = () => (
     <div className="relative w-full bg-red-50">
       {/* Referencia de video */}
       <div className="flex flex-col gap-8 items-center justify-center w-full h-[90vh]">
-        <h1 className="font-semibold text-center text-8xl">Cel-Fii Tecnología</h1>
-        <span className="text-5xl font-semibold text-red-500">Referencia de video</span>
+        <h1 className="font-semibold text-center text-8xl">Cel-Fii</h1>
+        <span className="text-5xl font-semibold text-red-500">Tecnología</span>
       </div>
       {/* <video
         className="object-cover w-full"
