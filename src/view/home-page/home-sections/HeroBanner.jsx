611a94044ee6d2c397c56c80d8479e9ecce0f020
María Sol <mikaiha888@gmail.com>
2024-11-22 20:34:52 -0300

    feat: add video and sound button

--- src/view/home-page/home-sections/HeroBanner.jsx
+++ src/view/home-page/home-sections/HeroBanner.jsx
@@ -1,26 +1,42 @@
-// import video from "../../../assets/celfii-video.mp4";
+import { useRef, useState } from "react";
+import { Volume2, VolumeOff } from 'lucide-react';
 
-const HeroBanner = () => (
-  <section>
-    <div className="relative w-full bg-red-50">
-      {/* Referencia de video */}
-      <div className="flex flex-col gap-8 items-center justify-center w-full h-[90vh]">
-        <h1 className="font-semibold text-center text-8xl">Cel-Fii</h1>
-        <span className="text-5xl font-semibold text-red-500">Tecnología</span>
+import video from "../../../assets/celfii-video.mp4";
+
+const HeroBanner = () => {
+  const [isMuted, setIsMuted] = useState(true);
+  const videoRef = useRef(null);
+
+  const toggleMute = () => {
+    if (videoRef.current) {
+      videoRef.current.muted = !videoRef.current.muted;
+      setIsMuted(videoRef.current.muted);
+    }
+  };
+
+  return (
+    <section>
+      <div className="relative w-full bg-red-50">
+        <video
+          ref={videoRef}
+          className="object-cover w-full"
+          style={{ height: "calc(100vh - 85px)" }}
+          autoPlay
+          loop
+          muted={isMuted}
+        >
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <button
+          onClick={toggleMute}
+          className="absolute p-3 text-white bg-gray-800 bg-opacity-50 rounded-sm top-4 right-4"
+        >
+          {isMuted ? <VolumeOff /> : <Volume2 />}
+        </button>
       </div>
-      {/* <video
-        className="object-cover w-full"
-        style={{ height: "calc(100vh - 80px)" }}
-        autoPlay
-        loop
-        muted
-      >
-        {" "}
-        <source src={video} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video> */}
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default HeroBanner;
